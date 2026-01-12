d0e8fdb1a4f1d1752ec2dc80818013d3babc2ca2
LanS <igor.geraskov@mail.ru>
2026-01-12 16:07:10 +0300

    Update create_session.py

--- parser/create_session.py
+++ parser/create_session.py
@@ -1,5 +1,5 @@
-api_id = '22077758'
-api_hash = 'bdd95b684ca60e44ef79a453a712f5cc'
+api_id = '34833012'
+api_hash = 'cbdd7731d08f01fb8b64a063d851e117'
 
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
