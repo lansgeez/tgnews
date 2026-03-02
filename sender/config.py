10b013477246284e4e39b30fec220a2b260ee0d0
LanS <igor.geraskov@mail.ru>
2026-03-02 17:24:26 +0300

    Update config.py

--- sender/config.py
+++ sender/config.py
@@ -5,4 +5,4 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TARGET_CHANNEL = os.getenv("TARGET_CHANNEL", "@test_tgnews")
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:29092")
-KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "news_filtered")  # <-- ВАЖНО
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "news_filtered") 
